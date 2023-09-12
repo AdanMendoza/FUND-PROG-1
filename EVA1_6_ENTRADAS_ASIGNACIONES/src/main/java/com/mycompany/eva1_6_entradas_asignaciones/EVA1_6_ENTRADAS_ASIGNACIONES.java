bca6ef73b859e69a0dba08c50e92cf128f3dd7fc
AdanMendoza <143568907+AdanMendoza@users.noreply.github.com>
2023-09-11 20:01:32 -0700

    Add files via upload

---
+++ EVA1_6_ENTRADAS_ASIGNACIONES/src/main/java/com/mycompany/eva1_6_entradas_asignaciones/EVA1_6_ENTRADAS_ASIGNACIONES.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_6_entradas_asignaciones;
+
+/**
+ *
+ * @author ADRIANA ROMERO
+ */
+public class EVA1_6_ENTRADAS_ASIGNACIONES {
+
+    public static void main(String[] args) {
+String marca;
+String modelo;
+int año;
+Scanner capturota = new Scanner(System.in);
+System.out.println("Introduce la la marca del auto");
+marca = capturota.nextLine();
+System.out.println("Introduce el modelo del auto");
+modelo = capturota.nextLine();
+System.out.println("Introduce el año del auto");
+año = capturota.nextLine();
+System.out.println(marca);
+System.out.println(modelo);
+System.out.println(año);
+
+
+
+    }
+}
