0fd4ebcddb7b7d43af32bdaa521089ae5476a06a
AdanMendoza <143568907+AdanMendoza@users.noreply.github.com>
2023-10-30 18:28:24 -0700

    Add files via upload

---
+++ EVA2_14_CICLO_FOR_2/src/eva2_14_ciclo_for_2/EVA2_14_CICLO_FOR_2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_14_ciclo_for_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_14_CICLO_FOR_2 {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      // TODO code application logic here
+      int noCalifas;
+      int sumaCalifas = 0;//cero es el neutro aditivo, 1 es el neutro multiplicativo
+      Scanner input = new Scanner(System.in);
+      System.out.println("Introduce el número de calificaciones: ");
+      noCalifas = input.nextInt();
+      for(int i = 0; i < noCalifas; i++){
+         System.out.println("Introduce la calificación: ");
+         int califa = input.nextInt();
+         //sumatoria de calificaciones:
+         //ACUMULADORES
+         //sumaCalifas = sumaCalifas + califa;
+         sumaCalifas += califa;
+      }
+      //el promedio es sumaCalifas, pero ambos son enteros
+      //y dividir enteros en Java da como resultado un entero, sin decimal
+      double promedio = sumaCalifas / (noCalifas * 1.0);
+      System.out.println("El promedio es: " + promedio);
+   }
+           
+   
+}
